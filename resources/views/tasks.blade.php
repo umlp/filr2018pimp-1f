6e57657b997dd8e11fb2a662b5f01c4325b19645
david-garza-medina <35848686+david-garza-medina@users.noreply.github.com>
2018-05-24 22:46:58 +0200

    Maj

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -11,41 +11,32 @@
                 <div class="panel-body">
                     <!-- Display Validation Errors -->
                     @include('common.errors')
-
+                
                     <!-- Menu principal -->
                     <div class="row">
                         <div class="sm-1"></div>
-                        <div class="sm-2">
-                            <button type="button" class="btn btn-info">Aide</button>
+                        <div class="sm-8"></div>
+                            <div class="btn-group btn-group-justified" role="group">
+                              <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-primary">Equation du 1er degrée</button>
+                              </div>
+                              <div class="sm-2"></div>
+                              <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-primary">Equation du 2éme degrée</button>
+                              </div>
+                            </div>
+                            <div class="btn-group btn-group-justified" role="group">
+                              <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-primary">Derivée</button>
+                              </div>
+                              <div class="sm-2"></div>
+                              <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-primary">Primitive</button>
+                              </div>
+                            </div>
                         </div>
-                        <div class="sm-9"></div>
-                    </div>
-                    <div class="row">
-                        <div class="btn-group btn-group-justified" role="group">
-                          <div class="sm-1"></div>
-                          <div class="sm-4 btn-group" role="group">
-                            <button type="button" class="btn btn-primary">Equation du 1er degrée</button>
-                          </div>
-                          <div class="sm-2"></div>
-                          <div class="sm-4 btn-group" role="group">
-                            <button type="button" class="btn btn-primary">Equation du 2éme degrée</button>
-                          </div>
-                          <div class="sm-1"></div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="btn-group btn-group-justified" role="group">
-                          <div class="sm-1"></div>
-                          <div class="sm-4 btn-group" role="group">
-                            <button type="button" class="btn btn-primary">Derivée</button>
-                          </div>
-                          <div class="sm-2"></div>
-                          <div class="sm-4 btn-group" role="group">
-                            <button type="button" class="btn btn-primary">Primitive</button>
-                          </div>
-                          <div class="sm-1"></div>
-                        </div>
-                    </div>   
+                        <div class="sm-1"></div>
+                    </div>    
                     <div class="row">
                         <div class="sm-12">
                             <button type="button" class="btn btn-default">Tracer une fonction</button>
