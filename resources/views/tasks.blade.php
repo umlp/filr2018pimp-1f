bbe791fe65db7cf0e5dd2284a76ee13b8d7f3983
david-garza-medina <35848686+david-garza-medina@users.noreply.github.com>
2018-05-24 22:25:21 +0200

    Maj

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    New Task
+                    Menu principal
                 </div>
 
                 <div class="panel-body">
@@ -21,7 +21,7 @@
                     <div class="row"></div>    
                     <div class="row">
                         <div class="sm-1"></div>
-                        <div class="sm-10"></div>
+                        <div>
                             <div class="btn-group btn-group-justified" role="group">
                               <div class="sm-4 btn-group" role="group">
                                 <button type="button" class="btn btn-primary">Equation du 1er degrée</button>
@@ -48,70 +48,8 @@
                             <button type="button" class="btn btn-default">Tracer une fonction</button>
                         </div>
                     </div>
-
-                    <!-- New Task Form
-                    <form action="{{ url('task')}}" method="POST" class="form-horizontal">
-                        {{ csrf_field() }}
-
-                        <!-- Task Name -->
-                        <div class="form-group">
-                            <label for="task-name" class="col-sm-3 control-label">Task</label>
-
-                            <div class="col-sm-6">
-                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
-                            </div>
-                        </div>
-
-                        <!-- Add Task Button --><!--
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-6">
-                                <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-btn fa-plus"></i>Add Task
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                    -->
                 </div>
             </div>
-
-            <!-- Current Tasks 
-            @if (count($tasks) > 0)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Current Tasks
-                    </div>
-
-                    <div class="panel-body">
-                        <table class="table table-striped task-table">
-                            <thead>
-                                <th>Task</th>
-                                <th>&nbsp;</th>
-                            </thead>
-                            <tbody>
-                                @foreach ($tasks as $task)
-                                    <tr>
-                                        <td class="table-text"><div>{{ $task->name }}</div></td>
-
-                                        <!-- Task Delete Button -->
-                                        <td>
-                                            <form action="{{ url('task/'.$task->id) }}" method="POST">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
-
-                                                <button type="submit" class="btn btn-danger">
-                                                    <i class="fa fa-btn fa-trash"></i>Delete
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            @endif
-            -->
         </div>
     </div>
 @endsection
