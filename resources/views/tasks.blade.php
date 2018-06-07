affa4fef910f5fa1ba1c4cde3c6079ee3523475c
david-garza-medina <35848686+david-garza-medina@users.noreply.github.com>
2018-06-07 17:48:52 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -16,7 +16,7 @@
                     <div class="row">
                         <div class="sm-1"></div>
                         <div class="sm-2">
-                            <button type="button" class="btn btn-info"><a href="test">Aide</a></button>
+                            <button type="button" class="btn btn-info">Aide<</button>
                         </div>
                         <div class="sm-9"></div>
                     </div>
@@ -25,13 +25,13 @@
                         <div class="sm-10">
                             <div class="btn-group btn-group-justified" role="group">
                               <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-primary">
+                                <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-equation-1';">
                                     <img src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A58.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
                                     Equation du 1er degrée
                                 </button>
                               </div>
                               <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-primary">
+                                <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-equation-2';">
                                     <img src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A11.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
                                     Equation du 2éme degrée
                                 </button>
@@ -39,13 +39,13 @@
                             </div>
                             <div class="btn-group btn-group-justified" role="group">
                               <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-primary">
+                                <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-derivee';">
                                     <img src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A25.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
                                     Derivée
                                 </button>
                               </div>
                               <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-primary">
+                                <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-primitive';">
                                     <img src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A40.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
                                     Primitive
                                 </button>
