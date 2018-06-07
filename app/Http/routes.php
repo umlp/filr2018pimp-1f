c0cc9f6549508ff0f5c8e0ac0f3f2c0a8547db7c
david-garza-medina <35848686+david-garza-medina@users.noreply.github.com>
2018-06-07 15:37:52 +0200

    testing routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,22 +27,8 @@ Route::group(['middleware' => ['web']], function () {
     /**
      * Add New Task
      */
-    Route::post('/task', function (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:255',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect('/')
-                ->withInput()
-                ->withErrors($validator);
-        }
-
-        $task = new Task;
-        $task->name = $request->name;
-        $task->save();
-
-        return redirect('/');
+    Route::get('/test', function (Request $request) {
+        return view('welcome');
     });
 
     /**
