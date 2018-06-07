9ddc462530ebf05b7ba993e74d60bff9ba26a507
david-garza-medina <35848686+david-garza-medina@users.noreply.github.com>
2018-06-07 17:27:48 +0200

    Create prendre-photo-primitive.blade.php

---
+++ resources/views/prendre-photo-primitive.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Menu principal
+                </div>
+
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
+                
+                    <!-- Menu principal -->
+                    <form>
+                      <input type="file" accept="image/*;capture=camera">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
