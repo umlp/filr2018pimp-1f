f8b8099121812f47a8c9aa199945c2c8f09ccebf
david-garza-medina <35848686+david-garza-medina@users.noreply.github.com>
2018-06-07 23:13:14 +0200

    Update prendre-photo-equation-1.blade.php

--- resources/views/prendre-photo-equation-1.blade.php
+++ resources/views/prendre-photo-equation-1.blade.php
@@ -13,6 +13,9 @@
                     @include('common.errors')
                 
                     <!-- Menu principal -->
+                    <p class="bg-primary">
+                        Equation du 1er degrée...
+                    </p>
                     <form class="form-horizontal" action="afficher-resultat" method="post">
                         <div class="form-group has-feedback">
                             <label class="control-label col-sm-3">Prendre photo</label>
