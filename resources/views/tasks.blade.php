6cf7e6880a0ce2fbfa6b19c41c73ade196c3ab75
david-garza-medina <35848686+david-garza-medina@users.noreply.github.com>
2018-06-07 23:08:14 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -13,39 +13,46 @@
                     @include('common.errors')
                 
                     <!-- Menu principal -->
-                    <button type="button" class="btn btn-info btn-block" onclick="location.href='https://equasnap.herokuapp.com/aide';">
-                        Aide
-                    </button>
                     <form class="form-horizontal">
-                        <div class="btn-group btn-group-justified" role="group">
-                          <div class="form-group has-feedback">
-                            <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-equation-1';">
-                                <img class="img-thumbnail" src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A58.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
-                                Equation du 1er degrée
+                        <div class="form-group has-feedback">
+                            <button type="button" class="btn btn-info btn-block" onclick="location.href='https://equasnap.herokuapp.com/aide';">
+                                Aide
                             </button>
-                          </div>
-                          <div class="form-group has-feedback">
-                            <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-equation-2';">
-                                <img class="img-thumbnail" src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A11.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
-                                Equation du 2éme degrée
-                            </button>
-                          </div>
                         </div>
-                        <div class="btn-group btn-group-justified" role="group">
-                          <div class="form-group has-feedback">
-                            <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-derivee';">
-                                <img class="img-thumbnail" src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A25.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
-                                Derivée
-                            </button>
-                          </div>
-                          <div class="form-group has-feedback">
-                            <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-primitive';">
-                                <img class="img-thumbnail" src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A40.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
-                                Primitive
-                            </button>
-                          </div>
-                        </div>
-                    </form> 
+                        <div class="form-group has-feedback">
+                            <div class="sm-1"></div>
+                            <div class="sm-10">
+                                <div class="btn-group btn-group-justified" role="group">
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-equation-1';">
+                                        <img class="img-thumbnail" src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A58.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
+                                        Equation du 1er degrée
+                                    </button>
+                                  </div>
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-equation-2';">
+                                        <img class="img-thumbnail" src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A11.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
+                                        Equation du 2éme degrée
+                                    </button>
+                                  </div>
+                                </div>
+                                <div class="btn-group btn-group-justified" role="group">
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-derivee';">
+                                        <img class="img-thumbnail" src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A25.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
+                                        Derivée
+                                    </button>
+                                  </div>
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn btn-primary" onclick="location.href='https://equasnap.herokuapp.com/prendre-photo-primitive';">
+                                        <img class="img-thumbnail" src="https://raw.githubusercontent.com/umlp/filr2018pimp-1f/master/resources/assets/Screenshot_2018-05-24_%2B22%3A58%3A40.png" style="display:block;text-align: center;margin-left: auto;margin-right: auto;height:100px">
+                                        Primitive
+                                    </button>
+                                  </div>
+                                </div>
+                            </div>
+                        </div> 
+                    </form>
                 </div>
             </div>
         </div>
