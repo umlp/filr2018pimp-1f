48ffb64aad72682585448b72d0ad18d668d8a879
david-garza-medina <35848686+david-garza-medina@users.noreply.github.com>
2018-06-07 23:58:41 +0200

    Update prendre-photo-equation-2.blade.php

--- resources/views/prendre-photo-equation-2.blade.php
+++ resources/views/prendre-photo-equation-2.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Menu principal
+                    <a href="/">Menu principal</a>
                 </div>
 
                 <div class="panel-body">
@@ -13,8 +13,21 @@
                     @include('common.errors')
                 
                     <!-- Menu principal -->
-                    <form>
-                      <input type="file" accept="image/*;capture=camera">
+                    <h1>
+                        Equation du 2éme degrée...
+                    </h1>
+                    <form class="form-horizontal" action="afficher-resultat" method="get">
+                        <div class="form-group has-feedback">
+                            <label class="control-label col-sm-3">Prendre photo</label>
+                            <div class="col-sm-9">
+                              <input class="form-control" type="file" accept="image/*;capture=camera">
+                            </div>
+                        </div>
+                        <div class="form-group has-feedback">
+                            <div class="col-sm-12">
+                                <input class="form-control" type="submit" value="Valider">
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
